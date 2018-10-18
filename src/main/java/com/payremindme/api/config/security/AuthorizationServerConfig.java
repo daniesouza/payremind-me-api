ef95c8c9bae55e0f3d42312e4d6a3f78a88dc7b9
Dnl <daniel.souza@eleevait.com.br>
2018-10-17 22:46:55 -0300

    security

--- src/main/java/com/payremindme/api/config/security/AuthorizationServerConfig.java
+++ src/main/java/com/payremindme/api/config/security/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.payremindme.api.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -28,6 +29,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private PasswordEncoder passwordEncoder;
 
     @Autowired
+    @Qualifier("appUserDetailsService")
     private UserDetailsService userDetailsService;
 
 
