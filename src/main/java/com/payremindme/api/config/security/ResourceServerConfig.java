74da9e28fe86487086e43bdadce02f76df5e015a
Dnl <daniel.souza@eleevait.com.br>
2018-10-17 23:28:45 -0300

    security

--- src/main/java/com/payremindme/api/config/security/ResourceServerConfig.java
+++ src/main/java/com/payremindme/api/config/security/ResourceServerConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecur
 
 @Profile("oauth-security")
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
