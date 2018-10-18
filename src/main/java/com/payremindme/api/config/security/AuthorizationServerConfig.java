3f45ac6ccee0806b07be14fd0e91edabfa53ebc3
Dnl <daniel.souza@eleevait.com.br>
2018-10-17 23:36:46 -0300

    security

--- src/main/java/com/payremindme/api/config/security/AuthorizationServerConfig.java
+++ src/main/java/com/payremindme/api/config/security/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,9 +16,9 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
 import java.util.Arrays;
 
+@Profile("oauth-security")
 @Configuration
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
@@ -67,7 +68,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public JwtAccessTokenConverter getAccessTokenConverter() {
         JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-        accessTokenConverter.setSigningKey("ESSA DIJ");
+        accessTokenConverter.setSigningKey("ESSA CHAVE NAO SERA QUEBRADA ASDASDASASDASD FDIAJFDIJ");
         return accessTokenConverter;
     }
 
